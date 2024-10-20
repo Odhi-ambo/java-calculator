350c68ef7ba9de0b25027db0f9f64c178bd87b94
Odhi-ambo <michaelderrick605@gmail.com>
2024-10-20 22:30:44 +0300

    initial commit simple calc using swing

---
+++ simpleCalculator.java
@@ -0,0 +1,16 @@
+
+import javax.swing.JFrame;
+
+
+public class simpleCalculator {
+
+    public static void main(String[] args) {
+        //creating jframe
+        JFrame frame = new JFrame("Simple Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setLayout(null);
+
+        
+    }
+}
